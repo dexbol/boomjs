869e25a030fbdf92d3bf67ad2d3fb5d033cd2135
dexbol <me@dexbol.com>
2011-05-19 18:39:17 +0800

    remove

--- test/lib.php
+++
@@ -1,8 +0,0 @@
-<?php
-    header('Content-Type','text/javascript');
-	header('Cache-Control','max-age=120');	
-	header("Content-Type", "application/x-javascript");
-	usleep(500000);
-	
-?>
-
--- test/test1.php
+++
@@ -1,9 +0,0 @@
-<?php
-    header('Content-Type','text/javascript');
-	header('Cache-Control','max-age=120');	
-	header("Content-Type", "application/x-javascript");
-	usleep(50000);
-	
-?>
-
-var t1='t1';
